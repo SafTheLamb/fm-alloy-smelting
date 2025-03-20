016c520ea65532d709e3e48c6dd515656f618c9d
sapphiriza <sapphiriza@gmail.com>
2025-03-20 15:22:08 -0700

    Version 0.1.2: Lead and Titanium compat fixes.

--- prototypes/compat/crushing-industry.lua
+++ prototypes/compat/crushing-industry.lua
@@ -11,17 +11,16 @@ if mods["crushing-industry"] then
     end
   end
 
+  ------------------------------------------------------------------------ Crushed BZ Ores
+
   if settings.startup["crushing-industry-ore"].value then
     if mods["bzlead"] and settings.startup["alloy-smelting-coke"].value then
-      frep.scale_ingredient("crushed-lead-smelting", "crushed-lead-ore", {amount=5})
-      frep.scale_result("crushed-lead-smelting", "lead-plate", {amount=5})
-      frep.add_ingredient("crushed-lead-smelting", {type="item", name="coke", amount=1})
-      frep.change_time("crushed-lead-smelting", {scale=2.5})
+      AlloySmelting.smelt_in_kiln("crushed-lead-smelting", {type="item", name="coke", amount=1}, 5)
     end
 
     if mods["bztitanium"] and settings.startup["alloy-smelting-coke"].value then
       if data.raw.recipe["crushed-titanium-smelting"] then
-        frep.add_ingredient("crushed-titanium-smelting", {type="item", name="coke", amount=2})
+        AlloySmelting.smelt_in_kiln("crushed-titanium-smelting", {type="item", name="coke", amount=2})
       end
     end
   end
