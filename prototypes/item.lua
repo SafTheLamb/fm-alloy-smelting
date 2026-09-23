6747caa775e1d840fcd65fe613f3b048ee158a78
Sapphira Riza <safthelamb@gmail.com>
2026-09-22 19:33:52 -0700

    fuel_categories

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -45,7 +45,7 @@ data:extend({
     subgroup = "raw-material",
     order = "b[chemistry]-A[coke]",
     hidden = not settings.startup["alloy-smelting-coke"].value,
-    fuel_category = "chemical",
+    fuel_categories = {"chemical"},
     fuel_value = settings.startup["alloy-smelting-coke-fuel-value"].value or "1.6MJ",
     fuel_emissions_multiplier = 1.5,
     inventory_move_sound = item_sounds.solid_fuel_inventory_move,
